b5ec462599b6d1b79563c14eeed0494f65a859db
John Dao <work@johndao.dev>
2021-05-18 00:47:54 +1000

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# youtube-unbiased
+# The Youtube Unbiased
+A simple app that allows users to watch youtube videos without their opinions being swayed by other sentiments.  
+  
+# Why I made it  
+The internet is a rough place, with many opinions being shared between a mass of people. Throughout my time browsing, I found such sentiments to increasingly obscure my own objectivity when seeing how good a video is, and this is when this idea crept into my head.  
+I wanted a way to watch videos in peace, with a way for my own opinion to not be swayed by a video that was heavily liked or disliked, with such a desire culminating in this light app that allow an experience to occur!  
+
+# What it removes  
+The app itself depends on page to both render and change while it loads new things.  
+Some things that it removes are:  
+- Views and like/dislike counts when watching videos (still allows you to like/dislike the video)  
+- Views from video suggestions  
+- The entire comment section (does not allow you to post your own comments)  
+- Subscriber count from both the video, and the channel's own page  
+  
+Please note that this is quite a basic app and I am planning to add more features in the near future!  
+  
+# How to use it  
+Find it on the chrome extensions store OR feel free to clone this repo and follow the instructions found [here](https://developer.chrome.com/docs/extensions/mv3/getstarted/)  
+  
+Have fun and thanks for checking this out :)
---
+++ script.js
@@ -0,0 +1,79 @@
+document.addEventListener("DOMSubtreeModified", handleRemoveDetails);
+
+function handleRemoveDetails() {
+    /**
+     * All document filters for browsing youtube as well as watching videos!
+     */
+    // comments
+    if (document.getElementById('comments')) {
+        document.getElementById('comments').remove();
+    }
+
+    // like bar details
+    if (document.getElementById('sentiment')) document.getElementById('sentiment').remove();
+
+    // view details
+    // views for video
+    if (document.getElementById('count')) {
+        // view count
+        document.getElementById('count').remove();
+        // the annoying dot
+        document.getElementById('dot').remove();
+    }
+
+    // like details
+    if (document.getElementsByClassName('style-scope ytd-toggle-button-renderer style-text')) {
+        Array.from(document.getElementsByClassName('style-scope ytd-toggle-button-renderer style-text')).forEach(function(item) {
+            if (item.id === 'text') {
+                item.remove();
+            }
+        });
+    }
+
+    // video metadata from suggestions
+    if (document.getElementsByClassName('style-scope ytd-video-meta-block')) {
+        Array.from(document.getElementsByClassName('style-scope ytd-video-meta-block')).forEach(function(item) {
+            if (item.textContent.includes('views')  && item.tagName === 'SPAN') {
+                item.remove();
+            }
+        });
+    }
+
+    // views from suggestions
+    if (document.getElementsByClassName('ytm-badge-and-byline-item-byline small-text')) {
+        Array.from(document.getElementsByClassName('ytm-badge-and-byline-item-byline small-text')).forEach(function(item) {
+            if (item.textContent.includes('views')  && item.tagName === 'SPAN') {
+                item.remove();
+            }
+        });
+    }
+
+
+    // supporter stuff (gets rid of all of the ticks and 'new' badges so that people have to select
+    // only based on the thumbnail and the channel
+    if (document.getElementsByClassName('style-scope ytd-badge-supported-renderer')) {
+        Array.from(document.getElementsByClassName('style-scope ytd-badge-supported-renderer')).forEach(function(item) {
+            item.remove();
+        });
+    }
+
+    // get rid of subscriber count
+    if (document.getElementById('owner-sub-count')) document.getElementById('owner-sub-count').remove();
+
+    /**
+     * All document filters for looking at an individual's channel
+     * Removes the sub count as well as the meta data for the video suggesions on the channel
+     */
+
+    // removing sub count
+    if (document.getElementById('subscriber-count')) document.getElementById('subscriber-count').remove();
+
+    // removing metadata from uploads
+    if (document.getElementsByClassName('style-scope ytd-grid-video-renderer')) {
+        Array.from(document.getElementsByClassName('style-scope ytd-grid-video-renderer')).forEach(function(item) {
+            if (item.textContent.includes('views') && item.tagName === 'SPAN') {
+                item.remove();
+            }
+        });
+    }
+}
